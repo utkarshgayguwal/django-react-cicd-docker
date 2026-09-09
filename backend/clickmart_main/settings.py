95b5547a09866136beb542c25a3151b5c0e06bfd
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2026-09-10 03:48:49 +0530

    Allowed host & environments added

--- backend/clickmart_main/settings.py
+++ backend/clickmart_main/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://16.171.67.195:5173'
+]
 
 # Application definition
 
