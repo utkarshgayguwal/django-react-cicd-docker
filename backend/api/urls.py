72acb1e51742630f94107606bed9cfcd13730c69
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2026-09-10 03:23:55 +0530

    docs: changes in docker-compose.yml

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,25 +15,16 @@ urlpatterns = [
     path('profile/', UserViews.ProfileView.as_view()),
 
     # Products APIs
-    # product list
     path('products/', ProductViews.ProductListView.as_view()),
-
-    # product detail
     path('products/<int:pk>/', ProductViews.ProductDetailView.as_view()),
 
     # Cart API
     path('cart/', CartViews.CartView.as_view()),
-
-    # Add to Cart
     path('cart/add/', CartViews.AddToCartView.as_view()),
-
-    # Manage Cart
     path('cart/items/<int:item_id>/', CartViews.ManageCartItemView.as_view()),
 
     # Orders
     path('orders/place/', OrderViews.PlaceOrderView.as_view()),
-
     path('orders/', OrderViews.MyOrdersView.as_view()),
-
     path('orders/<int:pk>/', OrderViews.OrderDetailView.as_view()),
 ]
